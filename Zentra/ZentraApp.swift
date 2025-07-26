211cf46d775d0ad8f84df6247446baf2cecb6015
Eric Sun <85909186+ericysun@users.noreply.github.com>
2025-07-25 19:40:56 -0700

    inital xcode commit files

---
+++ Zentra/ZentraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZentraApp.swift
+//  Zentra
+//
+//  Created by Guest    on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ZentraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZentraTests/ZentraTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZentraTests.swift
+//  ZentraTests
+//
+//  Created by Guest    on 7/24/25.
+//
+
+import Testing
+@testable import Zentra
+
+struct ZentraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
